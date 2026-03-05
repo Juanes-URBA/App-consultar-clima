7405c07377fbe8b23e0407d19a490973b853dee2
Juanes Urrego <024juanurrego@gmail.com>
2026-03-05 13:11:39 -0500

    fead(ui): agregar conponentes de la unterfaz de ususario

---
+++ scripts/ui.js
@@ -0,0 +1,20 @@
+
+const resultDiv = document.getElementById("result");
+
+export function showWeather(cityName, weather){
+    resultDiv.innerHTML =`
+    <div class="card">
+        <h2>${cityName}</h2>
+        <p><strong>Temperatura:</strong> ${weather.temperature} °C</p>
+        <p><strong>Viento:</strong> ${weather.windspeed} km/h</p>
+        <p><strong>Código clima:</strong> ${weather.weathercode}</p>
+        <p><strong>Hora:</strong> ${weather.time}</p>
+    </div>`; 
+}
+
+export function showError(message){
+    resultDiv.innerHTML =`
+    <div class="card>
+        <p style="color.red;"><strong>Error:</strong>${message}</p>
+    </div>`
+}
